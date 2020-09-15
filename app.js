8dce6ca1487d9d86cfdf125ed2963bc399d00923
Shalommoise <mshalom689@gmail.com>
2020-09-15 15:56:37 +0100

    added apiRouter

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const apiRouter = require("./routers/api.router");
+app.use(express.json());
 
-app.listen(9090, () => {
-  console.log("Lstening on 9090");
-});
+app.use("/api", apiRouter);
+
+// app.listen(9090, () => {
+//   console.log("Lstening on 9090");
+// });
+module.exports = app;
