782228f78e6a95bf5fc47b211d72ad662a838753
Shalommoise <mshalom689@gmail.com>
2020-09-15 11:11:38 +0100

    created utility function to make reference object

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,4 +1,4 @@
-const { randNumFunc } = require("../utils/utils");
+const { randNumFunc, makeRefObj, formatData } = require("../utils/utils");
 
 describe("randNumFunc", () => {
   it("return number", () => {
@@ -7,5 +7,40 @@ describe("randNumFunc", () => {
   it("return four digits", () => {
     expect(randNumFunc()).toBeLessThan(10000);
     expect(randNumFunc()).toBeGreaterThan(999);
+  });
+});
+
+describe("makeRefObj", () => {
+  it("returns one key value pair", () => {
+    const children = [
+      {
+        child_id: 1,
+        child_name: "Bob",
+        parent_email: "james@outlook.com",
+      },
+    ];
+    const output = { 1: { ["james@outlook.com"]: "Bob" } };
+    expect(makeRefObj(children)).toEqual(output);
+  });
+});
+describe.only("formatData", () => {
+  it("formats data", () => {
+    const children = [
+      {
+        child_id: 1,
+        child_name: "George",
+        parent_email: "james@outlook.com",
+      },
+    ];
+    const data = [
+      {
+        task_description: "do homework",
+        stars_worth: 6,
+        task_status: "pending",
+        child_name: "George",
+        parent_email: "james@outlook.com",
+      },
+    ];
+    expect(formatData(data, makeRefObj(children)));
   });
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,14 @@
 exports.randNumFunc = () => {
   return Math.floor(Math.random() * 10000);
 };
+
+exports.makeRefObj = (children) => {
+  const lookUpObj = {};
+  children.forEach(
+    (child) =>
+      (lookUpObj[child.child_id] = { [child.parent_email]: child.child_name })
+  );
+  return lookUpObj;
+};
+
+exports.formatData = (data, makeRefObj) => {};
