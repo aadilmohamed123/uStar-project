c6636bb9d56b1f3184788725d6efcaeb5ac35cfa
Aadil Mohamed <you@example.com>
2020-09-15 13:03:38 +0100

    made and tested makeRefObj & formatData

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -19,12 +19,56 @@ describe("makeRefObj", () => {
         parent_email: "james@outlook.com",
       },
     ];
-    const output = { 1: { ["james@outlook.com"]: "Bob" } };
+
+    const output = { ["james@outlook.com"]: [{ Bob: 1 } ]};
     expect(makeRefObj(children)).toEqual(output);
   });
+  it("returns multiple key value pairs", () => {
+    const children = [
+      {
+        child_id: 1,
+        child_name: "Bob",
+        parent_email: "james@outlook.com",
+        star_count: 5,
+      },
+      {
+        child_id: 2,
+        child_name: "Jim",
+        parent_email: "Danny@outlook.com",
+        star_count: 8,
+      },
+    ];
+    const output = {
+      ["james@outlook.com"]: [{ Bob: 1 }],
+      ["Danny@outlook.com"]: [{ Jim: 2 }]
+    };
+
+    expect(makeRefObj(children)).toEqual(output);
+  });
+    it("returns multiple key value pairs with same parents", () => {
+      const children = [
+        {
+          child_id: 1,
+          child_name: "Bob",
+          parent_email: "james@outlook.com",
+          star_count: 5,
+        },
+        {
+          child_id: 2,
+          child_name: "Jim",
+          parent_email: "james@outlook.com",
+          star_count: 8,
+        },
+      ];
+      const output = {
+        ["james@outlook.com"]: [{ Bob: 1 }, { Jim: 2 }]
+      };
+
+      expect(makeRefObj(children)).toEqual(output);
+    });
 });
-describe.only("formatData", () => {
-  it("formats data", () => {
+describe("formatData", () => {
+  it("formats data for single child", () => {
     const children = [
       {
         child_id: 1,
@@ -41,6 +85,59 @@ describe.only("formatData", () => {
         parent_email: "james@outlook.com",
       },
     ];
-    expect(formatData(data, makeRefObj(children)));
+    const output = [
+      {
+        task_description: "do homework",
+        stars_worth: 6,
+        task_status: "pending",
+        child_id: 1,
+      },
+    ];
+    expect(formatData(data, makeRefObj(children))).toEqual(output);
+  });
+  it("formats data for multiple children", () => {
+    const children = [
+      {
+        child_id: 1,
+        child_name: "George",
+        parent_email: "james@outlook.com",
+      },
+      {
+        child_id: 2,
+        child_name: "Jessica",
+        parent_email: "james@outlook.com",
+      },
+    ];
+    const data = [
+      {
+        task_description: "do homework",
+        stars_worth: 6,
+        task_status: "pending",
+        child_name: "George",
+        parent_email: "james@outlook.com",
+      },
+      {
+        task_description: "wash car",
+        stars_worth: 10,
+        task_status: "completed",
+        child_name: "Jessica",
+        parent_email: "james@outlook.com",
+      },
+    ];
+    const output = [
+      {
+        task_description: "do homework",
+        stars_worth: 6,
+        task_status: "pending",
+        child_id: 1,
+      },
+      {
+        task_description: "wash car",
+        stars_worth: 10,
+        task_status: "completed",
+        child_id: 2,
+      },
+    ];
+    expect(formatData(data, makeRefObj(children))).toEqual(output);
   });
 });
--- utils/utils.js
+++ utils/utils.js
@@ -4,11 +4,29 @@ exports.randNumFunc = () => {
 
 exports.makeRefObj = (children) => {
   const lookUpObj = {};
-  children.forEach(
-    (child) =>
-      (lookUpObj[child.child_id] = { [child.parent_email]: child.child_name })
-  );
+  children.forEach((child) => {
+    if (lookUpObj[child.parent_email]) {
+      lookUpObj[child.parent_email].push({
+        [child.child_name]: child.child_id,
+      });
+    } else {
+      lookUpObj[child.parent_email] = [{ [child.child_name]: child.child_id }];
+    }
+  });
   return lookUpObj;
 };
 
-exports.formatData = (data, makeRefObj) => {};
+exports.formatData = (data, lookupObj) => {
+  return data.map((entry) => {
+    const newEntry = { ...entry };
+    const { parent_email, child_name } = newEntry;
+    
+    newEntry.child_id = lookupObj[parent_email].find(
+      (childObj) => child_name === Object.keys(childObj)[0]
+    )[[child_name]];
+
+    delete newEntry.child_name;
+    delete newEntry.parent_email;
+    return newEntry;
+  });
+};
