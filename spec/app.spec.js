d9343e481f9d835b8fe49b58f71c4b7062ca1a34
Shalommoise <mshalom689@gmail.com>
2020-09-16 11:03:33 +0100

    testing for get rewards, post

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -28,7 +28,6 @@ describe("/api", () => {
           .expect(200)
           .then((res) => {
             res.body.children.forEach((child) => {
-     
               expect(child.parent_email).toBe("c@outlook.com");
               expect(Object.keys(child)).toEqual([
                 "child_id",
@@ -43,5 +42,91 @@ describe("/api", () => {
     });
   });
 
-  describe("/children", () => {});
+  describe("/children", () => {
+    describe("/:child_id", () => {
+      it("200 GET child by child id", () => {
+        return request(app)
+          .get("/api/children/3")
+          .expect(200)
+          .then((res) => {
+            const { child } = res.body;
+            expect(child.child_id).toBe(3);
+            expect(Object.keys(child)).toEqual([
+              "child_id",
+              "login_code",
+              "child_name",
+              "parent_email",
+              "star_count",
+            ]);
+          });
+      });
+      describe("/tasks", () => {
+        it("200 GET tasks by child_id", () => {
+          return request(app)
+            .get("/api/children/3/tasks")
+            .expect(200)
+            .then((res) => {
+              const { tasks } = res.body;
+              tasks.forEach((task) => {
+                expect(Object.keys(task)).toEqual([
+                  "task_id",
+                  "child_id",
+                  "task_description",
+                  "task_status",
+                  "stars_worth",
+                ]);
+              });
+            });
+        });
+        it("201 POST new task by child_id", () => {
+          return request(app)
+            .post("/api/children/3/tasks")
+            .send({ task_description: "wash clothes by hand", stars_worth: 20 })
+            .expect(201)
+            .then((res) => {
+              const { task } = res.body;
+              expect(task.task_description).toBe("wash clothes by hand");
+              expect(task.stars_worth).toBe(20);
+              expect(task.task_status).toBe("outstanding");
+              expect(task.child_id).toBe(3);
+            });
+        });
+      });
+      describe("/rewards", () => {
+        it("200 GET rewards by child_id ", () => {
+          return request(app)
+            .get("/api/children/3/rewards")
+            .expect(200)
+            .then((res) => {
+              const { rewards } = res.body;
+              rewards.forEach((reward) => {
+                expect(Object.keys(reward)).toEqual([
+                  "reward_id",
+                  "child_id",
+                  "reward_description",
+                  "star_cost",
+                ]);
+              });
+            });
+        });
+        it.only("201 POST rewards by Child_id", () => {
+          return request(app)
+            .post("/api/children/5/rewards")
+            .send({ reward_description: "Go to movies", star_cost: 10 })
+            .expect(201)
+            .then((res) => {
+              const { reward } = res.body;
+              expect(reward.child_id).toBe(5);
+              expect(reward.reward_description).toBe("Go to movies");
+              expect(reward.star_cost).toBe(10);
+            });
+        });
+      });
+    });
+  });
+  describe("/tasks", () => {
+    it("204 DELETE task by task_id", () => {
+      return request(app).delete("/api/tasks/4").expect(204);
+    });
+  });
 });
