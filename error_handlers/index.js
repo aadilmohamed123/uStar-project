2218b365999e6c8ea54aceb67c1bdab45761584d
Shalommoise <mshalom689@gmail.com>
2020-09-17 11:23:27 +0100

    PSQL erro handler - forbidden error

--- error_handlers/index.js
+++ error_handlers/index.js
@@ -3,3 +3,11 @@ exports.customErrorHandler = (err, req, res, next) => {
     res.status(err.status).send({ msg: err.msg });
   } else next(err);
 };
+
+exports.PSQLErrorHandler = (err, req, res, next) => {
+  if ((err.code = "23503"))
+    res.status(403).send({
+      msg: "403 Error: please remove children from account first and try again",
+    });
+  else next(err);
+};
