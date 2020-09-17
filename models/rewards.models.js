0c5ece31d2e265951329e98cc193a8922d3ecbf5
Shalommoise <mshalom689@gmail.com>
2020-09-17 15:03:15 +0100

    404 error handlers

--- models/rewards.models.js
+++ models/rewards.models.js
@@ -1,21 +1,21 @@
 const connection = require("../db/connection");
 
 exports.fetchRewardsByChildId = (child_id) => {
-  return connection
+  const rewards = connection
     .select("*")
     .from("rewards")
     .where("child_id", child_id)
-    .returning("*")
-    .then((res) => {
-      if (res.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: "404 Error: Not found",
-        });
-      } else {
-        return res;
-      }
-    });
+    .returning("*");
+
+  return Promise.all([rewards, checkChildrenExists(child_id)]).then(
+    (returningRows) => {
+      const [array, childExists] = returningRows;
+
+      return childExists
+        ? array
+        : Promise.reject({ status: 404, msg: "404 Error: Not found" });
+    }
+  );
 };
 
 exports.createReward = (child_id, reward_description, star_cost) => {
@@ -33,6 +33,26 @@ exports.removeReward = (reward_id) => {
     .where("reward_id", reward_id)
     .del()
     .then((res) => {
+      if (res === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "404 Error: Not found",
+        });
+      }
       return res;
+    });
+};
+
+const checkChildrenExists = (child_id) => {
+  return connection
+    .select("*")
+    .from("children")
+    .where("child_id", child_id)
+    .then((rows) => {
+      if (rows.length !== 0) {
+        return true;
+      } else {
+        return false;
+      }
     });
 };
--- models/tasks.models.js
+++ models/tasks.models.js
@@ -1,8 +1,22 @@
 const connection = require("../db/connection");
 const { updateChild } = require("./children.models");
 
-exports.fetchTasksByChildId = (id) => {
-  return connection.select("*").from("tasks").where("child_id", id);
+exports.fetchTasksByChildId = (child_id) => {
+  const tasks = connection
+    .select("*")
+    .from("tasks")
+    .where("child_id", child_id)
+    .returning("*");
+
+  return Promise.all([tasks, checkChildrenExists(child_id)]).then(
+    (returningRows) => {
+      const [array, childExists] = returningRows;
+
+      return childExists
+        ? array
+        : Promise.reject({ status: 404, msg: "404 Error: Not found" });
+    }
+  );
 };
 
 exports.createTask = (child_id, task_description, stars_worth) => {
@@ -21,6 +35,8 @@ exports.removeTask = (task_id) => {
     .where("task_id", task_id)
     .del()
     .then((res) => {
+      if (res === 0)
+        return Promise.reject({ status: 404, msg: "404 Error: Not found" });
       return res;
     });
 };
@@ -30,7 +46,23 @@ exports.updateTask = (task_id, task_status) => {
     .update("task_status", task_status)
     .returning("*")
     .then((res) => {
+      if (res.length === 0)
+        return Promise.reject({ status: 404, msg: "404 Error: Not found" });
       const [updatedTask] = res;
       return updatedTask;
+    });
+};
+
+const checkChildrenExists = (child_id) => {
+  return connection
+    .select("*")
+    .from("children")
+    .where("child_id", child_id)
+    .then((rows) => {
+      if (rows.length !== 0) {
+        return true;
+      } else {
+        return false;
+      }
     });
 };
